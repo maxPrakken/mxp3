3dbd78431f73b7af07c768baf9967709a2e86cf8
maxPrakken <maxprakken3@hotmail.nl>
2017-10-30 15:26:30 +0100

    added flip to player movement

--- Game/player.cpp
+++ Game/player.cpp
@@ -10,7 +10,7 @@ Player::Player() : Entity()
 
 	speed = 150;
 
-	flip = (SDL_RendererFlip)(SDL_FLIP_HORIZONTAL | SDL_FLIP_VERTICAL);
+	flip = SDL_FLIP_NONE;	/* example (SDL_RendererFlip)(SDL_FLIP_HORIZONTAL | SDL_FLIP_VERTICAL);*/
 }
 
 Player::~Player()
@@ -28,15 +28,19 @@ void Player::movementController(double deltatime)
 {
 	if (Input::getInstance()->getKey(SDLK_w)) {
 		pos -= Vector2(0, speed) * deltatime;
+		flip = SDL_FLIP_VERTICAL;
 	}
 	if (Input::getInstance()->getKey(SDLK_a)) {
 		pos -= Vector2(speed, 0) * deltatime;
+		flip = SDL_FLIP_HORIZONTAL;
 	}
 	if (Input::getInstance()->getKey(SDLK_s)) {
 		pos += Vector2(0, speed) * deltatime;
+		flip = SDL_FLIP_NONE;
 	}
 	if (Input::getInstance()->getKey(SDLK_d)) {
 		pos += Vector2(speed, 0) * deltatime;
+		flip = SDL_FLIP_NONE;
 	}
 }
 
