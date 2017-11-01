f61c2916283738dde50d06fa5043f67e3c8b9588
maxPrakken <maxprakken3@hotmail.nl>
2017-11-01 10:34:01 +0100

    sword position

--- Game/player.cpp
+++ Game/player.cpp
@@ -15,7 +15,7 @@ Player::Player() : Entity()
 	sword = new Entity();
 	sword->texturePath = "assets/INA.tga";
 	addchild(sword);
-	sword->pos = Vector2(150, 100);
+	sword->pos = Vector2(50, 0);
 
 	hasSlashed = false;
 
@@ -37,17 +37,28 @@ void Player::movementController(double deltatime)
 {
 	if (Input::getInstance()->getKey(SDLK_w)) {
 		pos -= Vector2(0, speed) * deltatime;
+
+		sword->pos = Vector2(0, -50);
 	}
 	if (Input::getInstance()->getKey(SDLK_a)) {
 		pos -= Vector2(speed, 0) * deltatime;
 		flip = SDL_FLIP_HORIZONTAL;
+
+		sword->flip = SDL_FLIP_HORIZONTAL;
+		sword->pos = Vector2(-50, 0);
 	}
 	if (Input::getInstance()->getKey(SDLK_s)) {
 		pos += Vector2(0, speed) * deltatime;
+
+		sword->flip = SDL_FLIP_NONE;
+		sword->pos = Vector2(0, 50);
 	}
 	if (Input::getInstance()->getKey(SDLK_d)) {
 		pos += Vector2(speed, 0) * deltatime;
 		flip = SDL_FLIP_NONE;
+
+		sword->flip = SDL_FLIP_NONE;
+		sword->pos = Vector2(50, 0);
 	}
 }
 
